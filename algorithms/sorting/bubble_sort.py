6ea0c7de1ec19eb9580593b877463b08107b3847
Sajal Tiwari <sajal.tiwari05@gmail.com>
2023-12-19 21:53:53 +0530

    golder

--- algorithms/sorting/bubble_sort.py
+++ algorithms/sorting/bubble_sort.py
@@ -1,17 +1,17 @@
-"""
-sort an array of N integers.
-"""
-from typing import List
+# """
+# sort an array of N integers.
+# """
+# # from typing import List
 
 
-input_array = [13,46,24,52,20,9]
-ARRAY_LENGTH = len(input_array)
+# input_array = [13,46,24,52,20,9]
+# ARRAY_LENGTH = len(input_array)
 
-def selection_sort(array: List[int]) -> List[int]:
-    '''swap number in sequence.'''
-    # for i in range(ARRAY_LENGTH):
-    #     pass
+# # def selection_sort(array: List[int]) -> List[int]:
+# #     '''swap number in sequence.'''
+# #     # for i in range(ARRAY_LENGTH):
+# #     #     pass
 
-    return None
+# #     return None
 
-print(selection_sort(array=input_array))
+# # print(selection_sort(array=input_array))
